0d86eaff5364f2172d5d6873496757b180619cd2
Muhammad Hisham Akram <36224246+GD-A-150880@users.noreply.github.com>
2020-12-09 16:06:50 +0500

    hisham

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    web allication
                 </div>
 
                 <div class="links">
